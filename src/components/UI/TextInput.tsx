a5109a669f215602900295bd81d3f204a2f0eac7
Богдан <brazbogdan@gmail.com>
2025-03-09 21:12:11 +0200

    updated commit

--- src/components/UI/TextInput.tsx
+++ src/components/UI/TextInput.tsx
@@ -2,16 +2,16 @@ import { CustomInputProps } from "../../types";
 
 const TextInput: React.FC<CustomInputProps> = ({ label, name, register, required, pattern, error }) => {
   return (
-    <div className="mt-2">
-      <label className="block font-semibold">{label}</label>
+    <div className="mt-4">
+      <label className="block text-lg font-medium text-gray-700">{label}</label>
       <input
-        className={`w-full border-2 rounded-lg hover:border-violet focus:border-violet outline-none p-2 ${
-          error ? "border-red" : ""
+        className={`w-full border-2 rounded-md p-3 mt-1 focus:ring-2 focus:ring-blue-500 focus:border-blue-500 ${
+          error ? "border-red-500" : "border-gray-300"
         }`}
         {...register(name, { required, pattern })}
         placeholder={label}
       />
-      <p className={`${error ? "text-red" : ""}`}>
+      <p className={`mt-1 text-sm ${error ? "text-red-500" : "text-gray-500"}`}>
         {error?.type === "pattern" ? "Only A-z characters, 2-12 length" : "This field is required"}
       </p>
     </div>
