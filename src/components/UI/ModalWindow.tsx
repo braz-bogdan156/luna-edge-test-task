35fccdab972af29ef0bd819e4cac588cb7d735a9
Богдан <brazbogdan@gmail.com>
2025-03-09 21:00:36 +0200

    updated commit

--- src/components/UI/ModalWindow.tsx
+++ src/components/UI/ModalWindow.tsx
@@ -1,21 +1,18 @@
-import {ModalProps} from "../../types";
+import { ModalProps } from "../../types";
 
-const ModalWindow = ({active, setActive, children}:ModalProps) => {
-    return (
-        <div
-            className={`w-screen h-screen bg-black/40 fixed top-0 left-0
-                flex items-center justify-center
-                scale-0 ${active? 'scale-100': ''} `}
-            onClick={() => setActive(false)}
-        >
-            <div
-                className="p-5 rounded-2xl bg-white "
-                onClick={e => e.stopPropagation()}
-            >
-                {children}
-            </div>
-        </div>
-    );
+const ModalWindow: React.FC<ModalProps> = ({ active, setActive, children }) => {
+  return (
+    <div
+      className={`w-screen h-screen bg-black/40 fixed top-0 left-0 flex items-center justify-center ${
+        active ? "scale-100" : "scale-0"
+      }`}
+      onClick={() => setActive(false)}
+    >
+      <div className="p-5 rounded-2xl bg-white" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default ModalWindow;
--- src/components/UI/TextInput.tsx
+++ src/components/UI/TextInput.tsx
@@ -1,20 +1,21 @@
-import {CustomInputProps} from "../../types";
+import { CustomInputProps } from "../../types";
 
-const TextInput = ({ label, name, register, required, pattern, error }: CustomInputProps) => {
+const TextInput: React.FC<CustomInputProps> = ({ label, name, register, required, pattern, error }) => {
+  return (
+    <div className="mt-2">
+      <label className="block font-semibold">{label}</label>
+      <input
+        className={`w-full border-2 rounded-lg hover:border-violet focus:border-violet outline-none p-2 ${
+          error ? "border-red" : ""
+        }`}
+        {...register(name, { required, pattern })}
+        placeholder={label}
+      />
+      <p className={`${error ? "text-red" : ""}`}>
+        {error?.type === "pattern" ? "Only A-z characters, 2-12 length" : "This field is required"}
+      </p>
+    </div>
+  );
+};
 
-    return (
-        <div className="mt-2">
-            <label className="block font-semibold">{label}</label>
-            <input
-                className={`w-full border-2 rounded-lg hover:border-violet focus:border-violet outline-none p-2
-                ${error ? 'border-red' : ''}`}
-                {...register(name, { required, pattern: pattern})} placeholder={label}
-            />
-            <p className={`${error ? 'text-red' : ''}`}>
-                {error?.type === 'pattern' ? 'Only A-z characters, 2-12 length' :
-                    'This field is required'}
-            </p>
-        </div>
-    );
-}
 export default TextInput;
